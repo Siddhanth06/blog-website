348ddae22f9060cfe4a14f4cb66b03fe99aee611
Siddhanth0629 <147157031+Siddhanth0629@users.noreply.github.com>
2024-12-12 12:31:04 +0530

    routes-setup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import userRouter from "./routes/user.route.js";
+import commentRouter from "./routes/comment.route.js";
+import postRouter from "./routes/comment.route.js";
+
 const app = express();
-console.log(process.env.test);
+
+app.use("/users", userRouter);
+app.use("/comments", commentRouter);
+app.use("/posts", postRouter);
 
 app.listen(3000, () => {
   console.log("running");
